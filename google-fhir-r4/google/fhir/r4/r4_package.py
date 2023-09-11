a22b420dafe8193b34c45be4e3d5bcce289586b0
Walt Askew <waltaskew@google.com>
2023-09-11 15:47:30 -0700

    internal change
    PiperOrigin-RevId: 564524124

--- google-fhir-r4/google/fhir/r4/r4_package.py
+++ google-fhir-r4/google/fhir/r4/r4_package.py
@@ -14,7 +14,7 @@
 # limitations under the License.
 """Loader for retrieving FHIR packages based on FHIR R4."""
 
-from typing import BinaryIO
+from typing import BinaryIO, Iterable
 
 from google.fhir.r4.proto.core.resources import code_system_pb2
 from google.fhir.r4.proto.core.resources import search_parameter_pb2
@@ -23,6 +23,7 @@ from google.fhir.r4.proto.core.resources import value_set_pb2
 from google.fhir.core.utils import fhir_package
 from google.fhir.r4 import primitive_handler
 
+# pyformat: disable
 import io
 import os
 
@@ -30,6 +31,7 @@ def _base_core_factory() -> BinaryIO:
   path = os.path.join(
       os.path.abspath(os.path.dirname(__file__)), 'data/hl7.fhir.r4.core.tgz')
   return io.open(path, 'rb')
+# pyformat: enable
 
 _PRIMITIVE_HANDLER = primitive_handler.PrimitiveHandler()
 
@@ -66,4 +68,64 @@ def load(
       search_parameter_pb2.SearchParameter,
       code_system_pb2.CodeSystem,
       value_set_pb2.ValueSet,
+  )
+
+
+def from_iterables(
+    ig_info: fhir_package.IgInfo,
+    structure_definitions: Iterable[
+        structure_definition_pb2.StructureDefinition
+    ],
+    search_parameters: Iterable[search_parameter_pb2.SearchParameter],
+    code_systems: Iterable[code_system_pb2.CodeSystem],
+    value_sets: Iterable[value_set_pb2.ValueSet],
+    resource_time_zone: str = 'Z',
+) -> fhir_package.FhirPackage[
+    structure_definition_pb2.StructureDefinition,
+    search_parameter_pb2.SearchParameter,
+    code_system_pb2.CodeSystem,
+    value_set_pb2.ValueSet,
+]:
+  """Builds a FHIR R4 `FhirPackage` containing the given resources.
+
+  Args:
+    ig_info: The metadata to associate with the `FhirPackage`.
+    structure_definitions: The structure definitions to include in the
+      `FhirPackage`.
+    search_parameters: The search parameters to include in the `FhirPackage`.
+    code_systems: The code systems to include in the `FhirPackage`.
+    value_sets: The value sets to include in the `FhirPackage`.
+    resource_time_zone: If additional JSON resources are added to the
+      `FhirPackage`, the time zone code to parse resource dates into when adding
+      those JSON resources.
+
+  Returns:
+    A `FhirPackage` instance with the requested resources.
+  """
+  return fhir_package.FhirPackage(
+      ig_info=ig_info,
+      structure_definitions=fhir_package.ResourceCollection.from_iterable(
+          structure_definitions,
+          structure_definition_pb2.StructureDefinition,
+          _PRIMITIVE_HANDLER,
+          resource_time_zone,
+      ),
+      search_parameters=fhir_package.ResourceCollection.from_iterable(
+          search_parameters,
+          search_parameter_pb2.SearchParameter,
+          _PRIMITIVE_HANDLER,
+          resource_time_zone,
+      ),
+      code_systems=fhir_package.ResourceCollection.from_iterable(
+          code_systems,
+          code_system_pb2.CodeSystem,
+          _PRIMITIVE_HANDLER,
+          resource_time_zone,
+      ),
+      value_sets=fhir_package.ResourceCollection.from_iterable(
+          value_sets,
+          value_set_pb2.ValueSet,
+          _PRIMITIVE_HANDLER,
+          resource_time_zone,
+      ),
   )
